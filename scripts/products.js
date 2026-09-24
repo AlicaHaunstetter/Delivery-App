3f38b2b51e4b5afc1534b143a228bc14e3826ffe
Alica Haunstetter <haunstetter.alica@gmail.com>
2026-09-24 08:48:03 +0200

    products.js added

---
+++ scripts/products.js
@@ -0,0 +1,108 @@
+let burgers = [
+  {
+    productNumber: 1000,
+    name: "Veggie Mushroom Black Burger",
+    description: "Mixed green salad, Tomatoes, Edamame, Mushrooms",
+    price: 16.9,
+    addedToBasket: true,
+    quantity: 1,
+  },
+  {
+    productNumber: 1001,
+    name: "All Meat Burger",
+    description: "Beef, Bacon, Dill pickles, Smoked cheese, Ketchup, BBQ sauce",
+    price: 15.9,
+    addedToBasket: false,
+    quantity: 0,
+  },
+  {
+    productNumber: 1002,
+    name: "Beef Red Burger",
+    description: "Beef, Cheese, Tomatoes, Lettuce, Onion",
+    price: 14.9,
+    addedToBasket: false,
+    quantity: 0,
+  },
+  {
+    productNumber: 1003,
+    name: "Big Chicken Burger",
+    description: "Chicken, Cheese, Tomatoes, Lettuce, Onion, Bell pepper",
+    price: 15.9,
+    addedToBasket: false,
+    quantity: 0,
+  },
+];
+
+let pizzas = [
+  {
+    productNumber: 2000,
+    name: "Pizza Margherita",
+    description: "Tomato Sauce, Mozzarella",
+    price: 11.9,
+    addedToBasket: true,
+    quantity: 1,
+  },
+  {
+    productNumber: 2001,
+    name: "Pizza Chorizo",
+    description: "Tomato slices, Mozzarella, Chorizo",
+    price: 13.9,
+    addedToBasket: false,
+    quantity: 0,
+  },
+  {
+    productNumber: 2002,
+    name: "Pizza Funghi",
+    description: "Red onion, Olives, Button Mushrooms, Mozzarella",
+    price: 12.9,
+    addedToBasket: false,
+    quantity: 0,
+  },
+  {
+    productNumber: 2003,
+    name: "Pizza Quattro Formaggi with Chicken",
+    description:
+      "Chicken, Mozzarella, Gorgonzola, Fontina, Parmigiano Reggiano",
+    price: 15.9,
+    addedToBasket: false,
+    quantity: 0,
+  },
+];
+
+let salads = [
+  {
+    productNumber: 3000,
+    name: "Warm Beef Arugula Salad",
+    description:
+      "Beef, Arugula, Field salad, Greek feta, Cherry tomatoes, Sun-dried Tomatoes, Balsamic-vinegar dressing",
+    price: 16.9,
+    addedToBasket: false,
+    quantity: 0,
+  },
+  {
+    productNumber: 3001,
+    name: "Mini Green Salad",
+    description: "Green salad, Cucumber, Carrots, Parsley, Radishes",
+    price: 7.9,
+    addedToBasket: true,
+    quantity: 1,
+  },
+  {
+    productNumber: 3002,
+    name: "Green Salad with Sea Food",
+    description:
+      "Mixed greens, Cherry tomatoes, Red onion, Mussels, Squid rings, Shrimp, Dijon mustard-lemon dressing with dill",
+    price: 16.9,
+    addedToBasket: false,
+    quantity: 0,
+  },
+  {
+    productNumber: 3003,
+    name: "Vegan Green Salad with Tofu",
+    description:
+      "Green salad, Cherry tomatoes, Cucumber, Baby spinach, Edamame, Radishes, Bittercress, Tofu, Peanuts",
+    price: 14.9,
+    addedToBasket: false,
+    quantity: 0,
+  },
+];
---
+++ scripts/script.js
@@ -0,0 +1,14 @@
+function init() {
+  renderBurger();
+  renderPizza();
+  renderSalad();
+  renderBasket();
+}
+
+function renderBurger() {}
+
+function renderPizza() {}
+
+function renderSalad() {}
+
+function renderBasket() {}
